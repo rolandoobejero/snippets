1067333927ecb26245e0e735c76340e1cd54e456
rolandoobejero <60249889+rolandoobejero@users.noreply.github.com>
2020-01-24 13:29:12 +0800

    Update remove-cart-item-ajax.js

--- remove-cart-item-ajax.js
+++ remove-cart-item-ajax.js
@@ -1,47 +1,47 @@
-// TEST// Ajax delete product in the cart
-		$(document).on('click', '.elementor-menu-cart__product-remove>a', function (e)
-		{
-		    e.preventDefault();
+// Ajax delete product in the cart
+$(document).on('click', '.elementor-menu-cart__product-remove>a', function (e)
+{
+    e.preventDefault();
+
+	$(this).parent().addClass('removing');
+
+    var product_id = $(this).attr("data-product_id"),
+	cart_item_key = $(this).attr("data-cart_item_key"),
+	product_container = $(this).parents('.mini_cart_item');
+
+    // Add loader
+    product_container.block({
+	message: null,
+	overlayCSS: {
+	    cursor: 'none'
+	}
+    });
+
+    $.ajax({
+	type: 'POST',
+	dataType: 'json',
+	url: wc_add_to_cart_params.ajax_url,
+	data: {
+	    action: "product_remove",
+	    product_id: product_id,
+	    cart_item_key: cart_item_key
+	},
+	success: function(response) {
+	    if ( ! response || response.error )
+		return;
+
+	    var fragments = response.fragments;
+
+	    // Replace fragments
+	    if ( fragments ) {
+		$.each( fragments, function( key, value ) {
+		    $( key ).replaceWith( value );
+		});
+	    }
 
 			$(this).parent().addClass('removing');
+			$('body').trigger( 'wc_fragments_refreshed' );
+	}
+    });
 
-		    var product_id = $(this).attr("data-product_id"),
-		        cart_item_key = $(this).attr("data-cart_item_key"),
-		        product_container = $(this).parents('.mini_cart_item');
-
-		    // Add loader
-		    product_container.block({
-		        message: null,
-		        overlayCSS: {
-		            cursor: 'none'
-		        }
-		    });
-
-		    $.ajax({
-		        type: 'POST',
-		        dataType: 'json',
-		        url: wc_add_to_cart_params.ajax_url,
-		        data: {
-		            action: "product_remove",
-		            product_id: product_id,
-		            cart_item_key: cart_item_key
-		        },
-		        success: function(response) {
-		            if ( ! response || response.error )
-		                return;
-
-		            var fragments = response.fragments;
-
-		            // Replace fragments
-		            if ( fragments ) {
-		                $.each( fragments, function( key, value ) {
-		                    $( key ).replaceWith( value );
-		                });
-		            }
-
-					$(this).parent().addClass('removing');
-					$('body').trigger( 'wc_fragments_refreshed' );
-		        }
-		    });
-
-		});
+});
